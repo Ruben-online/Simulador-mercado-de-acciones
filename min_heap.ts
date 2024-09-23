d301bb3c19e0668dbda9fe4a4d39791698ad790b
Ruben-online <78996839+Ruben-online@users.noreply.github.com>
2024-09-23 17:13:23 -0600

    update en los métodos, insertar y extractmin, creación de métodos auxiliares

--- min_heap.ts
+++ min_heap.ts
@@ -8,4 +8,42 @@ class MinHeap {
         this.heap = new Array(size + 1);
         this.n = 0;
     }
+    
+    public insert(order: Order): void {
+        this.n++;
+        this.heap[this.n] = order;
+        this.swim(this.n);
+    }
+
+    public extractMin(): Order | undefined {
+        if (this.n === 0) return undefined;
+        const min = this.heap[1];
+        this.swap(1, this.n);
+        this.n--;
+        this.sink(1);
+        return min;
+    }
+
+    private swim(k: number): void {
+        while (k > 1 && this.heap[Math.floor(k / 2)].getPrice() > this.heap[k].getPrice()) {
+            this.swap(k, Math.floor(k / 2));
+            k = Math.floor(k / 2);
+        }
+    }
+    
+    private sink(k: number): void {
+        while (2 * k <= this.n) {
+            let j = 2 * k;
+            if (j < this.n && this.heap[j].getPrice() > this.heap[j + 1].getPrice()) j++;
+            if (this.heap[k].getPrice() <= this.heap[j].getPrice()) break;
+            this.swap(k, j);
+            k = j;
+        }
+    }
+    
+    private swap(i: number, j: number): void {
+        const temp = this.heap[i];
+        this.heap[i] = this.heap[j];
+        this.heap[j] = temp;
+    }
 }
